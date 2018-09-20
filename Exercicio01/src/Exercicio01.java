2d6c16225e250775d1a725a4dfcae4f2d72a89b9
Carlos <chs.ponciano@gmail.com>
2018-09-20 15:29:22 -0300

    Melhorado algoritmo 1.a

--- Exercicio01/src/Exercicio01.java
+++ Exercicio01/src/Exercicio01.java
@@ -9,26 +9,26 @@ public class Exercicio01 {
         Exercicio01 t = new Exercicio01();
         int[][] adj = new int[4][4];
         //Não dirigido
-        adj[0][0] = 0;
-        adj[0][1] = 1;
-        adj[0][2] = 1;
-        adj[0][3] = 0;
-
-        adj[1][0] = 1;
-        adj[1][1] = 0;
-        adj[1][2] = 0;
-        adj[1][3] = 1;
-
-        adj[2][0] = 1;
-        adj[2][1] = 0;
-        adj[2][2] = 0;
-        adj[2][3] = 1;
-
-        adj[3][0] = 0;
-        adj[3][1] = 1;
-        adj[3][2] = 1;
-        adj[3][3] = 0;
-        System.out.println((t.isSimetrico(adj)) ? "Não dirigido" : "Dirigido");
+//        adj[0][0] = 0;
+//        adj[0][1] = 1;
+//        adj[0][2] = 1;
+//        adj[0][3] = 0;
+//
+//        adj[1][0] = 1;
+//        adj[1][1] = 0;
+//        adj[1][2] = 0;
+//        adj[1][3] = 1;
+//
+//        adj[2][0] = 1;
+//        adj[2][1] = 0;
+//        adj[2][2] = 0;
+//        adj[2][3] = 1;
+//
+//        adj[3][0] = 0;
+//        adj[3][1] = 1;
+//        adj[3][2] = 1;
+//        adj[3][3] = 0;
+//        System.out.println((t.isSimetrico(adj)) ? "Não dirigido" : "Dirigido");
 
         //Dirigido
         adj[0][0] = 0;
@@ -50,23 +50,39 @@ public class Exercicio01 {
         adj[3][1] = 0;
         adj[3][2] = 1;
         adj[3][3] = 0;
-        System.out.println((t.isSimetrico(adj)) ? "Não dirigido" : "Dirigido");
-    }
 
-    //Todo grafo simetrico é não-dirigido
-    private boolean isSimetrico(final int[][] adj) {
-        for (int v = 0; v < adj.length; v++) {
-            for (int w = 0; w < adj[v].length; w++) {
-                if (adj[v][w] != adj[w][v]) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        System.out.println(t.tipoDoGrafo(adj));
     }
 
     public String tipoDoGrafo(final int[][] adj) {
-        return null;
+        //0 - dirigido(false) ou não-dirigido(true);        
+        //1 - simples(false) ou multigrafo(true);        
+        //2 - regular;
+        //3 - completo;        
+        //4 - nulo ou bipartido;
+        boolean[] resultado = {true, false, false, false, false};
+
+        for (int v = 0; v < adj.length; v++) {
+            for (int w = 0; w < adj[v].length; w++) {
+                if (resultado[0]) {
+                    resultado[0] = (adj[v][w] != adj[w][v]);
+                }
+
+                if (!resultado[1]) {
+                    resultado[1] = (((v == w) && (adj[v][w] != 0)) || (adj[v][w] > 1));
+                }
+
+            }
+        }
+
+        //Saida
+        String str = (resultado[0]) ? "Não-Dirigido, " : "Dirigido, ";
+        str += (resultado[1]) ? "Multigrafo, " : "Simples, ";
+        str += (resultado[2]) ? "Regular, " : "";
+        str += (resultado[3]) ? "Completo, " : "";
+        str += (resultado[4]) ? "Nulo." : "Bipartido.";
+
+        return str;
     }
 
     public String arestasDoGrafo(final int[][] adj) {
@@ -74,6 +90,19 @@ public class Exercicio01 {
     }
 
     public String grausDoVertices(final int[][] adj) {
-        return null;
+        String str = "Graus: ";
+
+        return str;
+    }
+
+    private boolean isMultigrafo(final int[][] adj) {
+        for (int v = 0; v < adj.length; v++) {
+            for (int w = 0; w < adj[v].length; w++) {
+                if (((v == w) && (adj[v][w] != 0)) || (adj[v][w] > 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
