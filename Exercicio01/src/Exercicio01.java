c3df67550988588017d5e44eb5f122d8c2c9b68c
Carlos <chs.ponciano@gmail.com>
2018-09-20 16:47:39 -0300

    Construção da primeira etapa grafo regular 1.a

--- Exercicio01/src/Exercicio01.java
+++ Exercicio01/src/Exercicio01.java
@@ -28,28 +28,30 @@ public class Exercicio01 {
         adj[3][1] = 1;
         adj[3][2] = 1;
         adj[3][3] = 0;
+        System.out.println(t.tipoDoGrafo(adj));
+
+        System.out.println("\n-----------------------\n");
 
         //Dirigido
-//        adj[0][0] = 0;
-//        adj[0][1] = 1;
-//        adj[0][2] = 0;
-//        adj[0][3] = 1;
-//
-//        adj[1][0] = 0;
-//        adj[1][1] = 0;
-//        adj[1][2] = 0;
-//        adj[1][3] = 0;
-//
-//        adj[2][0] = 0;
-//        adj[2][1] = 1;
-//        adj[2][2] = 0;
-//        adj[2][3] = 0;
-//
-//        adj[3][0] = 0;
-//        adj[3][1] = 0;
-//        adj[3][2] = 1;
-//        adj[3][3] = 0;
+        adj[0][0] = 0;
+        adj[0][1] = 1;
+        adj[0][2] = 0;
+        adj[0][3] = 1;
 
+        adj[1][0] = 0;
+        adj[1][1] = 0;
+        adj[1][2] = 0;
+        adj[1][3] = 0;
+
+        adj[2][0] = 0;
+        adj[2][1] = 1;
+        adj[2][2] = 0;
+        adj[2][3] = 0;
+
+        adj[3][0] = 0;
+        adj[3][1] = 1;
+        adj[3][2] = 1;
+        adj[3][3] = 0;
         System.out.println(t.tipoDoGrafo(adj));
     }
 
@@ -59,8 +61,8 @@ public class Exercicio01 {
         //2 - regular;
         //3 - completo;        
         //4 - nulo ou bipartido;
-        boolean[] resultado = {true, false, false, false, false};
-        
+        boolean[] resultado = {true, false, true, false, false};
+
         for (int v = 0; v < adj.length; v++) {
             for (int w = 0; w < adj[v].length; w++) {
                 if (resultado[0]) {
@@ -73,6 +75,26 @@ public class Exercicio01 {
             }
         }
 
+        if (resultado[0]) {
+            int[] g = listaDeGrausNaoDirigido(adj);
+            for (int i = 1; i < g.length; i++) {
+                if (g[i - 1] != g[i]) {
+                    resultado[2] = false;
+                    break;
+                }
+            }
+        } else {
+            int[][] g = listaDeGrausDirigido(adj);
+            for (int i = 0; i < g.length; i++) {
+                for (int j = 1; j < g[i].length; j++) {
+                    if (g[i][j - 1] != g[i][j]) {
+                        resultado[2] = false;
+                        break;
+                    }
+                }
+            }
+        }
+
         //Saida
         String str = (resultado[0]) ? "Não-Dirigido, " : "Dirigido, ";
         str += (resultado[1]) ? "Multigrafo, " : "Simples, ";
@@ -81,6 +103,33 @@ public class Exercicio01 {
         str += (resultado[4]) ? "Nulo." : "Bipartido.";
 
         return str;
+    }
+
+    private int[] listaDeGrausNaoDirigido(final int[][] adj) {
+        int[] graus = new int[adj.length];
+
+        for (int v = 0; v < adj.length; v++) {
+            for (int w = 0; w < adj[v].length; w++) {
+                graus[v] += adj[v][w];
+            }
+        }
+
+        return graus;
+    }
+
+    private int[][] listaDeGrausDirigido(final int[][] adj) {
+        int[][] graus = new int[2][adj.length];
+
+        for (int v = 0; v < adj.length; v++) {
+            for (int w = 0; w < adj[v].length; w++) {
+                if (adj[v][w] > 0) {
+                    graus[0][v] += adj[v][w]; //Saida
+                    graus[1][w] += adj[v][w]; //Entrada
+                }
+            }
+        }
+
+        return graus;
     }
 
     public String arestasDoGrafo(final int[][] adj) {
