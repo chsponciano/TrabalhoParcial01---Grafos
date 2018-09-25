f267fa355a9aba3add75b6615bdcce666ad5c8b2
Vinícius <vinicius.lds.br@gmail.com>
2018-09-25 15:59:57 -0300

    Algoritmo de busca ewm largura da questão 2

--- Exercicio02/src/Executor.java
+++ Exercicio02/src/Executor.java
@@ -10,10 +10,12 @@
  */
 public class Executor {    
     public static void main(String[] args) {
-        String t = "10 11\nB A\nEntrada A\nB GT\nGT H\nH *\nB *\n* C\nC I\nI D\nC D\nD Saida";
+        String t;
+        t = "10 11\nB A\nEntrada A\nB GT\nGT H\nH *\nB *\n* C\nC I\nI D\nC D\nD Saida";
+        t = "16 20\nEntrada A\nA F\nF C\nC B\nB D\nC D\nF J\nJ H\nH G\nJ G\nJ *\n* I\nI L\nL M\nM K\nK Saida\nA K\nC E\nE I\nI M";
         Grafo g = new Grafo();
         g.montarGrafo(t);
-        g.buscar();
+        System.out.println("Total: " + g.buscar());
     }
     
 }
--- Exercicio02/src/Grafo.java
+++ Exercicio02/src/Grafo.java
@@ -1,9 +1,12 @@
 
-import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  *
@@ -14,18 +17,43 @@ public class Grafo {
     private Map<String, Vertice> vertices;
     private int qtdVertices;
     private int qtdArestas;
+    private int total;
 
     public Grafo() {
         this.vertices = new HashMap<>();
         
     }
 
-    public int buscar(){
-        return buscar(this.vertices.get("Entrada"));
+    public int buscar() {
+        this.total = 0;
+        Queue<Vertice> fila = new LinkedBlockingQueue<>();
+        fila.add(vertices.get("Entrada"));
+        int ateQueijo = this.buscar(fila, vertices.get("*"));
+        total = 0;
+        fila.clear();
+        fila.add(vertices.get("*"));
+        int ateSaida = this.buscar(fila, vertices.get("Saida"));
+        return ateQueijo + ateSaida;
     }
     
-    private int buscar(Vertice inicial){
-        return 0;
+    
+    private int buscar(Queue<Vertice> vertices, Vertice destino){
+        Queue<Vertice> fila = new LinkedBlockingQueue<>();
+        Vertice v;
+        while(!vertices.isEmpty()) {
+            v = vertices.poll();
+            if(destino.getIdentificacao().equals(v.getIdentificacao())) {
+                return total;
+            } else {
+                botaAdjacentesNaFila(v, fila);
+            }
+        }
+        total++;
+        return buscar(fila, destino);
+    }
+    
+    private void botaAdjacentesNaFila(Vertice v, Queue<Vertice> q) {
+        q.addAll(v.parentes());
     }
     
     public void montarGrafo(String entrada) {
@@ -37,7 +65,7 @@ public class Grafo {
         this.qtdVertices = Integer.parseInt(info[0]);
         this.qtdArestas = Integer.parseInt(info[1]);
 
-        for (int i = 2, j = 3; j < info.length; i++, j++) {
+        for (int i = 2, j = 3; j < info.length; i += 2, j += 2) {
             v1 = this.vertices.get(info[i]);
             v2 = this.vertices.get(info[j]);
 
